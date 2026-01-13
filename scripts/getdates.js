066c77e331c915702412e98036c6046cc1d8c186
tklegendary47 <tklegendary47@gmail.com>
2026-01-13 18:19:12 +0200

    Light Dark Toggle removed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,17 +1,5 @@
 const yearSpan = document.getElementById("currentyear");
 const modified = document.getElementById("lastModified");
-const toggle = document.getElementById("themeToggle");
 
-// Footer dates
 yearSpan.textContent = new Date().getFullYear();
 modified.textContent = `Last Modified: ${document.lastModified}`;
-
-// Theme toggle (dark ↔ light)
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("light-mode");
-
-  // Update icon based on current mode
-  toggle.textContent = document.body.classList.contains("light-mode")
-    ? "🌙"  // switch to dark
-    : "☀️"; // switch to light
-});
