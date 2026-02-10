e7c5c28ee7ddc084bf5e9efe85c502c1070909c1
tklegendary47 <tklegendary47@gmail.com>
2026-02-10 18:48:12 +0200

    eh em

--- project/project-scripts/form.js
+++ project/project-scripts/form.js
@@ -1,11 +1,32 @@
 /* PRODUCT LIST */
 const products = [
-  {id:"fc-1888", name:"Flux Capacitor"},
-  {id:"fc-2050", name:"Power Laces"},
-  {id:"fs-1987", name:"Time Circuits"},
-  {id:"ac-2000", name:"Low Voltage Reactor"},
-  {id:"jj-1969", name:"Warp Equalizer"}
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
 ];
+
 
 const select = document.getElementById("product");
 products.forEach(p => {
